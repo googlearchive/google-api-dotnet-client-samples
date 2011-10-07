3ea92921b55de5e16e75ca97cd53873497b51ed8
David Waters <davidwaters@google.com>
2011-10-07 19:20:38 +0100

    Working not yet protected

--- SampleHelper/PromptingClientCredentials.cs
+++ SampleHelper/PromptingClientCredentials.cs
@@ -24,35 +24,37 @@ namespace Google.Apis.Samples.Helper
     {
         private const string ApplicationFolderName = "Google.Apis.Samples";
         private const string ClientCredentialsFileName = "client.dat";
-        private const string FileKeyApiKey ="ApiKey";
-        private const string FileKeyClientId ="ClientId";
+        private const string FileKeyApiKey = "ApiKey";
+        private const string FileKeyClientId = "ClientId";
         private const string FileKeyClientSecret = "ClientSecret";
         private const string PromptSimpleCreate = "Blah Blah Blah please create a Simple For First time";
 
 
-        private static FileInfo CredentialsFile {
-            get {
+        private static FileInfo CredentialsFile
+        {
+            get
+            {
                 string applicationDate = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-                string filePath =
-                    Path.Combine(Path.Combine(applicationDate, ApplicationFolderName), ClientCredentialsFileName);
-                return new FileInfo(filePath);
+                string googleAppDirectory = Path.Combine(applicationDate, ApplicationFolderName);
+                var directoryInfo = new DirectoryInfo(googleAppDirectory);
+                if (directoryInfo.Exists == false)
+                {
+                    directoryInfo.Create();
+                }
+                return new FileInfo(Path.Combine(googleAppDirectory, ClientCredentialsFileName));
             }
         }
 
-        public static void ClearClientCredentials() {
-            FileInfo clientCredentials = CredentialsFile;
-            if (clientCredentials.Exists) {
-                clientCredentials.Delete();
-            }
-        }
-
-        private static IDictionary<string, string> ParseFile() {
-            var parsedValues = new Dictionary<string,string>(5);
+        private static IDictionary<string, string> ParseFile()
+        {
+            var parsedValues = new Dictionary<string, string>(5);
             StreamReader sr = CredentialsFile.OpenText();
             string currentLine = sr.ReadLine();
-            while (currentLine != null) {
+            while (currentLine != null)
+            {
                 int firstEquals = currentLine.IndexOf('=');
-                if (firstEquals > 0 && firstEquals + 1 < currentLine.Length) {
+                if (firstEquals > 0 && firstEquals + 1 < currentLine.Length)
+                {
                     string key = currentLine.Substring(0, firstEquals);
                     string value = currentLine.Substring(firstEquals + 1);
                     parsedValues.Add(key, value);
@@ -63,21 +65,27 @@ namespace Google.Apis.Samples.Helper
             return parsedValues;
         }
 
-        public static SimpleClientCredentials CreateSimpleClientCredentials() {
+        private static SimpleClientCredentials CreateSimpleClientCredentials()
+        {
             SimpleClientCredentials simpleCredentials =
                 CommandLine.CreateClassFromUserinput<SimpleClientCredentials>();
-            using (FileStream fStream = CredentialsFile.OpenWrite()) {
-                using(TextWriter tw = new StreamWriter(fStream)) {
-                    tw.WriteLine("{0}={1}",FileKeyApiKey,simpleCredentials.ApiKey);
+            using (FileStream fStream = CredentialsFile.OpenWrite())
+            {
+                using (TextWriter tw = new StreamWriter(fStream))
+                {
+                    tw.WriteLine("{0}={1}", FileKeyApiKey, simpleCredentials.ApiKey);
                 }
             }
             return simpleCredentials;
         }
 
-        public static FullClientCredentials CreateFullClientCredentials() {
+        private static FullClientCredentials CreateFullClientCredentials(bool isExtension)
+        {
             FullClientCredentials fullCredentials = CommandLine.CreateClassFromUserinput<FullClientCredentials>();
-            using (FileStream fStream = CredentialsFile.OpenWrite()) {
-                using(TextWriter tw = new StreamWriter(fStream)) {
+            using (FileStream fStream = CredentialsFile.OpenWrite())
+            {
+                using (TextWriter tw = new StreamWriter(fStream))
+                {
                     tw.WriteLine("{0}={1}", FileKeyApiKey, fullCredentials.ApiKey);
                     tw.WriteLine("{0}={1}", FileKeyClientId, fullCredentials.ClientId);
                     tw.WriteLine("{0}={1}", FileKeyClientSecret, fullCredentials.ClientSecret);
@@ -86,28 +94,64 @@ namespace Google.Apis.Samples.Helper
             return fullCredentials;
         }
 
-        public static SimpleClientCredentials EnsureSimpleClientCredentials() {
-            if (CredentialsFile.Exists == false) {
+        public static SimpleClientCredentials EnsureSimpleClientCredentials()
+        {
+            if (CredentialsFile.Exists == false)
+            {
                 return CreateSimpleClientCredentials();
             }
 
             IDictionary<string, string> values = ParseFile();
-            if(values.ContainsKey(FileKeyApiKey) == false) {
+            if (values.ContainsKey(FileKeyApiKey) == false)
+            {
                 return CreateSimpleClientCredentials();
             }
-            return new SimpleClientCredentials() {ApiKey = values[FileKeyApiKey]};
+            return new SimpleClientCredentials() { ApiKey = values[FileKeyApiKey] };
+        }
+
+        public static FullClientCredentials EnsureFullClientCredentials()
+        {
+            if (CredentialsFile.Exists == false)
+            {
+                return CreateFullClientCredentials(false);
+            }
+
+            IDictionary<string, string> values = ParseFile();
+            if (values.ContainsKey(FileKeyApiKey) == false || 
+                values.ContainsKey(FileKeyClientId) == false ||
+                values.ContainsKey(FileKeyClientSecret) == false)
+            {
+                return CreateFullClientCredentials(true);
+            }
+
+            return new FullClientCredentials() { 
+                ApiKey = values[FileKeyApiKey],
+                ClientId = values[FileKeyClientId], 
+                ClientSecret = values[FileKeyClientSecret]};
+
+        }
+
+        public static void ClearClientCredentials()
+        {
+            FileInfo clientCredentials = CredentialsFile;
+            if (clientCredentials.Exists)
+            {
+                clientCredentials.Delete();
+            }
         }
     }
 
-    public class FullClientCredentials : SimpleClientCredentials {
+    public class FullClientCredentials : SimpleClientCredentials
+    {
         [Description("Client ID as shown in the 'Client ID for installed applications' section")]
-        public string ClientId { get; set;}
+        public string ClientId;
         [Description("Client secret as shown in the 'Client ID for installed applications' section")]
-        public string ClientSecret { get; set; }
+        public string ClientSecret;
     }
 
-    public class SimpleClientCredentials {
+    public class SimpleClientCredentials
+    {
         [Description("API key as shown in the Simple API Access section.")]
-        public string ApiKey { get; set;}
+        public string ApiKey;
     }
 }
