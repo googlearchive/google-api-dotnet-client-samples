94ee44b76570e461e44c2530daae73b050eeacd7
Jesse McGrew <jmcgrew@google.com>
2015-07-21 17:14:43 -0700

    Added blank line in usings.

--- Calendar.ASP.NET.MVC5/Models/CalendarEventGroup.cs
+++ Calendar.ASP.NET.MVC5/Models/CalendarEventGroup.cs
@@ -16,6 +16,7 @@ limitations under the License.
 
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+
 using Google.Apis.Calendar.v3.Data;
 
 namespace Calendar.ASP.NET.MVC5.Models
